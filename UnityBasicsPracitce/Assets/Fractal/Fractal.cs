4d0b471ab53e9ed56d020a74a2856608cc573e41
QIQIQI <beycome@163.com>
2017-07-12 18:40:43 +0800

    Fractal Section End

--- UnityBasicsPracitce/Assets/Fractal/Fractal.cs
+++ UnityBasicsPracitce/Assets/Fractal/Fractal.cs
@@ -2,22 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fractal : MonoBehaviour 
+public class Fractal : MonoBehaviour
 {
-	public Mesh mesh;
-	public Material mat;
-
-	public int maxDepth;
-	[SerializeField]private int depth;
-	public float childScaler = 0.5f;
-
+	[Header("Resourses")]
+	public Mesh[] meshes;
+	public MaterialHolder mats;
 	public enum GenerationType {Instantly, Gradually}
 	public GenerationType generationType;
 
+	[Header("Depth")]
+	public int maxDepth;
+	[SerializeField]private int depth;
+
+	[Header("Shape")]
+	public float childScaler = 0.5f;
+	[Range(0f, 1f)]public float perfectRate = 1.0f;
+	public float maxTwistAngle = 30f;
+
+	[Header("Rotate")]
+	public bool useRotation = false;
+	public float maxRotateSpeed = 90f;
+	[SerializeField]private float rotateSpeed;
+
+	//for a parent cube, how many face direct out of all six directions has a child 
+	private static ChildGroup[] childGroups = {
+		new ChildGroup(Vector3.up, Quaternion.identity),
+		new ChildGroup(Vector3.right, Quaternion.Euler(0f, 0f, -90f)),
+		new ChildGroup(Vector3.left, Quaternion.Euler(0f, 0f, 90f)),
+		new ChildGroup(Vector3.forward, Quaternion.Euler(90f, 0f, 0f)),
+		new ChildGroup(Vector3.back, Quaternion.Euler(-90, 0f, 0f))
+	};
+		
 	void Start()
 	{
-		this.gameObject.AddComponent<MeshFilter>().mesh = mesh;
-		this.gameObject.AddComponent<MeshRenderer>().material = mat;
+		this.gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
+		this.gameObject.AddComponent<MeshRenderer>().material = mats.GetColoredMat();
 
 		if(depth < maxDepth)
 		{
@@ -37,32 +56,39 @@ public class Fractal : MonoBehaviour
 
 	IEnumerator CreateAllChildGradually()
 	{
-		yield return new WaitForSeconds(0.5f);
-		CreateSingleChild(Vector3.up);
-
-		yield return new WaitForSeconds(0.5f);
-		CreateSingleChild(Vector3.right);
+		for(int i = 0; i < childGroups.Length; i++)
+		{
+			yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+			CreateSingleChild(i);
+		}
 	}
 
 	void CreateAllChildInstantly()
 	{
-		CreateSingleChild(Vector3.up);
-		CreateSingleChild(Vector3.right);
-		CreateSingleChild(-Vector3.right);
-		CreateSingleChild(-Vector3.up);
+		for(int i = 0; i < childGroups.Length; i++)
+		{
+			CreateSingleChild(i);
+		}
 	}
 
-	void CreateSingleChild(Vector3 dir)
+	void CreateSingleChild(int index)
 	{
+		if(Random.Range(0f, 1.0f) > perfectRate)
+			return;
+
 		GameObject next = new GameObject("Child Node");
-		next.AddComponent<Fractal>().Init(this, dir);	//For new component, exexution order is : Awake - OnEnable - Init - Start
+		next.AddComponent<Fractal>().Init(this, index);	//For new component, exexution order is : Awake - OnEnable - Init - Start
 	}
 
-	void Init(Fractal parent, Vector3 dir)
+	void Init(Fractal parent, int index)
 	{
-		this.mesh = parent.mesh;
-		this.mat = parent.mat;
+		this.meshes = parent.meshes;
+		this.mats = parent.mats;
 		this.maxDepth = parent.maxDepth;
+		this.perfectRate = parent.perfectRate;
+		this.maxRotateSpeed = parent.maxRotateSpeed;
+		this.maxTwistAngle = parent.maxTwistAngle;
+		this.useRotation = parent.useRotation;
 		this.depth = parent.depth + 1;
 		this.generationType = parent.generationType;
 
@@ -70,6 +96,30 @@ public class Fractal : MonoBehaviour
 
 		this.childScaler = parent.childScaler;
 		this.transform.localScale = Vector3.one * childScaler;
-		this.transform.localPosition = dir * (0.5f + childScaler / 2f);	//note the 0.5f
+		this.transform.localPosition = childGroups[index].direction * (0.5f + childScaler / 2f);	//note the 0.5f
+		this.transform.localRotation = childGroups[index].orientation;
+
+		this.rotateSpeed = Random.Range(0f, maxRotateSpeed);
+		this.transform.Rotate(Random.Range(-maxTwistAngle, maxTwistAngle), 0f, 0f);
+	}
+
+	void Update()
+	{
+		if(!useRotation)
+			return;
+		
+		transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);	//this is NOT changing ratation Y value, BUT roate around Y axis
+	}
+}
+
+public class ChildGroup
+{
+	public Vector3 direction;
+	public Quaternion orientation;
+
+	public ChildGroup(Vector3 d, Quaternion o)
+	{
+		direction = d;
+		orientation = o;
 	}
 }
---
+++ UnityBasicsPracitce/Assets/Fractal/MaterialHolder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialHolder : MonoBehaviour 
+{
+	public Material baseMat;
+
+	private Material[] mats = new Material[7];
+
+	void Awake()
+	{
+		for(int i = 0; i < mats.Length; i++)
+		{
+			mats[i] = new Material(baseMat);
+		}
+
+		mats[0].color = Color.red;
+		mats[1].color = new Color(1f, 0.5f, 0f);
+		mats[2].color = Color.yellow;
+		mats[3].color = Color.green;
+		mats[4].color = Color.cyan;
+		mats[5].color = Color.blue;
+		mats[6].color = Color.magenta;
+	}
+
+	public Material GetColoredMat()
+	{
+		int x = Random.Range(0, mats.Length);
+
+		return mats[x];
+	}
+}
